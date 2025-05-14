90b716bb24c97f1248d833ef98632f25f9e770ca
SIVAKUMARGLEZIAn <sivakumarglezian@gmail.com>
2025-05-14 10:02:37 +0530

    Add files via upload

---
+++ viva.py
@@ -0,0 +1,39 @@
+print("welcome my banking app")
+#question01
+def users(name,age,gmail):
+    users={
+        101{"user_name":"kamal","age":21,"gmail":"kamal@gmail.com"}
+        102{"user_name":"vimal","age":22,"gmail":"vimal@gmail.com"}
+        103{"user_name":"nimal","age":23,"gmail":"nimal@gmail.com"}
+        }
+        total = 0
+for users in user_name:
+    total += users
+print("The total users is:", total)
+
+    lookup=int(input("enter the user id in lookup"))
+    if lookup_id in users:
+        print("the account holder found",users[lookup_id])
+    else:
+        ("the account holder not found")
+users(name,age,gmail)
+#question03
+with open file("users.txt","w")
+      file.write("user name:{user_name},\n")
+      file.write("user id:{user_id},\n")
+      file.write("gmail is:{gmail},\n")
+      file.close()
+#question04
+def account_creation():
+    user_id=int(input("enter the new id"))
+    user_name=(input("enter the new user name"))
+    user_gamil=(input("enter the new user gmail"))
+if the user_id==users[lookup_id]:
+    print("user_name already taken")
+else:
+    print("keep promoting until a unique username is entered")
+account_creation()
+
+ 
+
+    
